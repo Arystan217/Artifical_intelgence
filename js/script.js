a4c28291d9e768268bc7103cdef40d173336da66
Arystan217 <65996409+Arystan217@users.noreply.github.com>
2021-06-04 18:20:24 +0600

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  
-  // клик по бургер меню
-  $('.burger-icon').click(function(){
-    $(this).toggleClass('burger-icon--active');
-    $('.burger').toggleClass('burger--active');
-  });
-
-})
